afec419403c1063aa047544544e959551cdc12b2
richard ozo <rumdumdum@gmail.com>
2015-10-27 10:34:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,24 @@
-/*
-  basic-proxy.js: Basic example of proxying over HTTP
-  Copyright (c) Nodejitsu 2013
-  Permission is hereby granted, free of charge, to any person obtaining
-  a copy of this software and associated documentation files (the
-  "Software"), to deal in the Software without restriction, including
-  without limitation the rights to use, copy, modify, merge, publish,
-  distribute, sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so, subject to
-  the following conditions:
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+/*global process */
+var http = require('http');
+var express = require('express');
+var jsforceAjaxProxy = require('jsforce-ajax-proxy');
 
-var util = require('util'),
-    colors = require('colors'),
-    http = require('http'),
-    httpProxy = require('http-proxy');
+var app = express();
 
-var welcome = [
-  '#    # ##### ##### #####        #####  #####   ####  #    # #   #',
-  '#    #   #     #   #    #       #    # #    # #    #  #  #   # # ',  
-  '######   #     #   #    # ##### #    # #    # #    #   ##     #  ',   
-  '#    #   #     #   #####        #####  #####  #    #   ##     #  ',   
-  '#    #   #     #   #            #      #   #  #    #  #  #    #  ',   
-  '#    #   #     #   #            #      #    #  ####  #    #   #  '
-].join('\n');
+app.configure(function () {
+  app.set('port', process.env.PORT || 3123);
+});
 
-util.puts(welcome.rainbow.bold);
+app.configure('development', function () {
+  app.use(express.errorHandler());
+});
 
-//
-// Basic Http Proxy Server
-//
-httpProxy.createServer({
-  target:'http://localhost:9003'
-}).listen(8003);
+app.all('/proxy/?*', jsforceAjaxProxy({ enableCORS: true }));
 
-//
-// Target Http Server
-//
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
-  res.end();
-}).listen(9003);
+app.get('/', function(req, res) {
+  res.send('JSforce AJAX Proxy');
+});
 
-util.puts('http proxy server'.blue + ' started '.green.bold + 'on port '.blue + '8003'.yellow);
-util.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '9003 '.yellow);
+http.createServer(app).listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
+});
